8072017b12f18dff33f4ebd7f91f7a223dd27ade
André Maree <ammaree@users.noreply.github.com>
2022-03-10 16:37:02 +0200

    Fix to enable production build

--- FreeRTOS_Support.h
+++ FreeRTOS_Support.h
@@ -62,9 +62,13 @@ extern	EventGroupHandle_t	xEventStatus, TaskRunState,	TaskDeleteState ;
 
 // ##################################### global function prototypes ################################
 
+#if 1
+#define bRtosCheckStatus(X)	(((xEventGroupGetBits(xEventStatus) & X) == X) ? 1 : 0)
+#else
 inline bool bRtosCheckStatus(EventBits_t X) {
 	return ((xEventGroupGetBits(xEventStatus) & X) == X) ? 1 : 0;
 }
+#endif
 
 bool bRtosToggleStatus(const EventBits_t uxBitsToToggle) ;
 bool bRtosVerifyState(const EventBits_t uxTaskToVerify) ;
