4660f0d131604bfe4ff366111531965e77a75e8a
André Maree <ammaree@users.noreply.github.com>
2024-07-08 15:25:43 +0200

    Define API as static

--- FreeRTOS_Support.c
+++ FreeRTOS_Support.c
@@ -118,7 +118,7 @@ static TaskStatus_t	sTS[configFR_MAX_TASKS] = { 0 };
 	static SemaphoreHandle_t shTaskInfo;
 #endif
 
-TaskStatus_t * psRtosStatsFindWithNumber(UBaseType_t xTaskNumber) {
+static TaskStatus_t * psRtosStatsFindWithNumber(UBaseType_t xTaskNumber) {
 	IF_myASSERT(debugPARAM, xTaskNumber != 0);
 	IF_myASSERT(debugPARAM, xTaskNumber <= configFR_MAX_TASKS);
 	for (int i = 0; i <= NumTasks; ++i) {
