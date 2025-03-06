5b93361389708a82279a469051531c14043536b5
André Maree <ammaree@users.noreply.github.com>
2025-03-06 13:39:57 +0200

    Change from ioB?GET macro to API call

--- FreeRTOS_Support.h
+++ FreeRTOS_Support.h
@@ -22,7 +22,7 @@ extern "C" {
 #define	MALLOC_MARK()	u32_t y,x=xPortGetFreeHeapSize();
 #define	MALLOC_CHECK()	y=xPortGetFreeHeapSize();IF_TRACK(y<x,"%u->%u (%d)" strNL,x,y,y-x);
 
-#define MESSAGE(mess,...)	IF_PX(debugTRACK && ioB1GET(ioUpDown), mess, ##__VA_ARGS__)
+#define MESSAGE(mess,...)	IF_PX(debugTRACK && xOptionGet(ioUpDown), mess, ##__VA_ARGS__)
 #define TASK_START(name) 	MESSAGE("[%s] starting" strNL, name)
 #define TASK_STOP(name) 	MESSAGE("[%s] stopping" strNL, name)
 
