0f4e3f9bf4a80a0bb9d5c841bc7967ed941db098
André Maree <ammaree@users.noreply.github.com>
2025-05-07 15:34:58 +0200

    Add specific muxes to checking list

--- FreeRTOS_Support.c
+++ FreeRTOS_Support.c
@@ -39,8 +39,8 @@
 
 #if	(rtosSEMA_DEBUG > 0)
 
-SemaphoreHandle_t * MonitorList[] = { &shUARTmux, 		/* &shTaskInfo &SL_VarMux &SL_NetMux */ };
 SemaphoreHandle_t * pSHmatch = NULL;
+SemaphoreHandle_t * MonitorList[] = { &shUARTmux, &shSLvars, &shSLsock,		/* &shTaskInfo  */ };
 
 /**
  * @brief	match semaphore address provided against list entries
